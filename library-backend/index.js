d0b8eb9004f78f349fdce40af2b83eb980e3e2f8
Melxi <magomed.futijev@gmail.com>
2019-08-19 20:32:29 +0300

    8.4: kirjailijan kirjat

--- library-backend/index.js
+++ library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,7 +107,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      const byAuthor = book => book.author === args.author
+
+      return books.filter(byAuthor)
+    },
     allAuthors: () => authors
   },
   Author: {
