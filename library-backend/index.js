57da53d98b931acb4ddeb52446906a63a501fe38
Melxi <magomed.futijev@gmail.com>
2019-08-20 12:06:29 +0300

    8.7: Kirjailijan syntymävuoden päivitys

--- library-backend/index.js
+++ library-backend/index.js
@@ -110,6 +110,10 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ) : Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ) : Author
   }
 `
 
@@ -153,6 +157,18 @@ const resolvers = {
       }
       books = books.concat(book)
       return book
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...args, born: args.setBornTo}
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
